98cc677788c736ec6c0dcb4a2ef449ea197d0a03
Daniel-Nasciment <daniel.fecaf@outlook.com>
2020-12-27 18:02:25 -0300

    5- Registro automatico no banco de dados

--- src/main/java/com/daniel/cursojs/CursojsApplication.java
+++ src/main/java/com/daniel/cursojs/CursojsApplication.java
@@ -1,13 +1,36 @@
 package com.daniel.cursojs;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursojsApplication {
+import com.daniel.cursojs.domain.Categoria;
+import com.daniel.cursojs.repositories.CategoriaRepository;
 
+
+@SpringBootApplication
+public class CursojsApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
