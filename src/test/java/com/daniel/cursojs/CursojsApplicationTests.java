9ef135fa05a7ac379e5d85666d0e0e0395be92c7
Daniel-Nasciment <daniel.fecaf@outlook.com>
2020-12-27 13:15:57 -0300

    1- Projeto criado

---
+++ src/test/java/com/daniel/cursojs/CursojsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.cursojs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
