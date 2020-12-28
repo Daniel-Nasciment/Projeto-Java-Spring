ee29d85746b9abf5f64d951ec46120aa7da71df6
Daniel-Nasciment <daniel.fecaf@outlook.com>
2020-12-28 10:20:21 -0300

    8- Criando as Classes/Tabelas Cidade e Estado

--- src/main/java/com/daniel/cursojs/CursojsApplication.java
+++ src/main/java/com/daniel/cursojs/CursojsApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.cursojs.domain.Categoria;
+import com.daniel.cursojs.domain.Cidade;
+import com.daniel.cursojs.domain.Estado;
 import com.daniel.cursojs.domain.Produto;
 import com.daniel.cursojs.repositories.CategoriaRepository;
+import com.daniel.cursojs.repositories.CidadeRepository;
+import com.daniel.cursojs.repositories.EstadoRepository;
 import com.daniel.cursojs.repositories.ProdutoRepository;
 
 
@@ -22,6 +26,12 @@ public class CursojsApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojsApplication.class, args);
@@ -47,6 +57,17 @@ public class CursojsApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 	
