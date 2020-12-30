3dae7f3ed1826b386c8c70224f3c3a6db56ee710
Daniel-Nasciment <daniel.fecaf@outlook.com>
2020-12-30 16:53:35 -0300

    16- Criando POST da API

--- src/main/java/com/daniel/cursojs/resources/CategoriaResource.java
+++ src/main/java/com/daniel/cursojs/resources/CategoriaResource.java
@@ -1,12 +1,17 @@
 package com.daniel.cursojs.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.daniel.cursojs.domain.Categoria;
 import com.daniel.cursojs.services.CategoriaService;
@@ -25,7 +30,14 @@ public class CategoriaResource {
 		
 		return ResponseEntity.ok().body(obj);
 		
+	}
+
+	@RequestMapping(method = RequestMethod.POST) 
+	public ResponseEntity<Void> insert (@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
 		
+		return ResponseEntity.created(uri).build();
 		
 	}
 	
